eb4c839e744caa58a311258d87a94fe0f7c1a107
Ahmed <ahwork711@gmail.com>
2022-06-03 18:06:35 +0200

    3

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ function App() {
       />
       <Routes>
         <Route
-          path="/"
+          path=""
           element={
             <Home
               showContactModalUpdater={showContactModalUpdater}
