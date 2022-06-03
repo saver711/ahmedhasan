c4b4cabf0da7d61232d08e14e43bc6359fb2c028
Ahmed <ahwork711@gmail.com>
2022-06-03 17:54:59 +0200

    2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import {  HashRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter>
+  // for github pages
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
